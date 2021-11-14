5bd5e5cb4f52ccde3334278758cb1b7c1b3ca887
nlpming <qqchenzhiming@163.com>
2021-11-14 15:53:22 +0800

    update

--- deepfm_train.py
+++ deepfm_train.py
@@ -17,6 +17,37 @@ from deepctr.feature_column import SparseFeat, DenseFeat, get_feature_names
 from deepfm_config import sparse_features, dense_features
 from deepfm_config import read_file, generate_sparse_dict, get_sparse_index, generate_dense_dict, get_dense_norm
 
+def build_deepfm_model(sparse_features, dense_features, sparse_dict, dense_dict, params):
+    """构建deepfm模型"""
+
+    # 生成linear_feature_columns, dnn_feature_columns
+    linear_feature_columns = []
+    for feat in sparse_features:
+        if feat != 'user_id':
+            linear_feature_columns.append(SparseFeat(feat, vocabulary_size=len(sparse_dict[feat]) + 2,
+                                                     embedding_dim=params["embedding_dim"]))
+    for feat in dense_features:
+        linear_feature_columns.append(DenseFeat(feat, 1, ))
+
+    dnn_feature_columns = []
+    for feat in sparse_features:
+        if feat != 'user_id':
+            dnn_feature_columns.append(SparseFeat(feat, vocabulary_size=training_data[feat].max() + 2,
+                                                  embedding_dim=params["embedding_dim"]))
+    for feat in dense_features:
+        dnn_feature_columns.append(DenseFeat(feat, 1, ))
+
+    model = DeepFM(linear_feature_columns, dnn_feature_columns,
+                   task='binary',
+                   dnn_hidden_units=params['dnn_hidden_units'],
+                   l2_reg_linear=params['l2_reg_linear'],
+                   l2_reg_embedding=params['l2_reg_embedding'],
+                   l2_reg_dnn=params['l2_reg_dnn'],
+                   dnn_dropout=params['dnn_dropout'],
+                   dnn_activation=params['dnn_activation'],
+                   dnn_use_bn=params['dnn_use_bn'])
+    return model
+
 if __name__ == "__main__":
     # 1. DeepFM网络超参数
     params = {
@@ -71,46 +102,20 @@ if __name__ == "__main__":
         training_data[feat] = training_data[feat].apply(get_sparse_index, args=(sparse_dict, feat))
         valid_data[feat] = valid_data[feat].apply(get_sparse_index, args=(sparse_dict, feat))
 
-    # 4. 生成linear_feature_columns, dnn_feature_columns
-    linear_feature_columns = []
-    for feat in sparse_features:
-        if feat != 'user_id':
-            linear_feature_columns.append(SparseFeat(feat, vocabulary_size=training_data[feat].max() + 2,
-                                                     embedding_dim=params["embedding_dim"]))
-    for feat in dense_features:
-        linear_feature_columns.append(DenseFeat(feat, 1, ))
-
-    dnn_feature_columns = []
-    for feat in sparse_features:
-        if feat != 'user_id':
-            dnn_feature_columns.append(SparseFeat(feat, vocabulary_size=training_data[feat].max() + 2,
-                                                  embedding_dim=params["embedding_dim"]))
-    for feat in dense_features:
-        dnn_feature_columns.append(DenseFeat(feat, 1, ))
-
-    # 5. 构建模型输入数据
-    feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
+    # 4. 构建模型输入数据
+    feature_names = sparse_features + dense_features
     train_model_input = {name: np.array(training_data[name].values.tolist()) for name in feature_names}
     valid_model_input = {name: np.array(valid_data[name].values.tolist()) for name in feature_names}
 
-    # 6. 定义模型、优化器、损失函数、回调函数
-    model = DeepFM(linear_feature_columns, dnn_feature_columns,
-                   task='binary',
-                   dnn_hidden_units=params['dnn_hidden_units'],
-                   l2_reg_linear=params['l2_reg_linear'],
-                   l2_reg_embedding=params['l2_reg_embedding'],
-                   l2_reg_dnn=params['l2_reg_dnn'],
-                   dnn_dropout=params['dnn_dropout'],
-                   dnn_activation=params['dnn_activation'],
-                   dnn_use_bn=params['dnn_use_bn'])
-
+    # 5. 构建模型
+    model = build_deepfm_model(sparse_features, dense_features, sparse_dict, dense_dict, params)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=params['learning_rate']),
                loss=tf.keras.losses.BinaryCrossentropy(),
                metrics=[tf.keras.metrics.AUC()])
 
     # call back function
     checkpoint = ModelCheckpoint(
-	    model_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+	    model_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='min')
     reduce_lr = ReduceLROnPlateau(
 	    monitor='val_loss', factor=0.8, patience=2, min_lr=0.0001, verbose=1)
     earlystopping = EarlyStopping(
@@ -120,14 +125,14 @@ if __name__ == "__main__":
     callbacks = [checkpoint, reduce_lr, earlystopping, tensorboard]
 
     # 7. training过程
-    history = model.fit(train_model_input, training_data[target].values,
-                        validation_data=(valid_model_input, valid_data[target].values),
-                        batch_size=params['batch_size'], epochs=params['epochs'], verbose=2, shuffle=True, callbacks=callbacks)
+    model.fit(train_model_input, training_data[target].values,
+        validation_data=(valid_model_input, valid_data[target].values),
+        batch_size=params['batch_size'], epochs=params['epochs'], verbose=2, shuffle=True, callbacks=callbacks)
 
     # 8. predict过程
-    deepfm_model = tf.keras.models.load_model(model_path)
+    model.load_weight(model_path)
 
-    pred_ans = deepfm_model.predict(valid_model_input, batch_size=params['batch_size']) #预测结果为正样本概率值
+    pred_ans = model.predict(valid_model_input, batch_size=params['batch_size']) #预测结果为正样本概率值
     print("valid LogLoss", round(log_loss(valid_data[target].values, pred_ans), 6))
     print("valid AUC", round(roc_auc_score(valid_data[target].values, pred_ans), 6))
 
